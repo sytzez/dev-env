3cdf64534ca486978aa686e97a7edabc40d82760
sytzez <sytzez@gmail.com>
2024-04-08 13:13:25 +0100

    Change lualine theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -23,7 +23,8 @@ return {
 
             return {
                 options = {
-                    theme = "auto",
+                    -- theme = "auto",
+                    theme = "iceberg_dark",
                     globalstatus = true,
                     disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
                 },
