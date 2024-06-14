cdedbe8fbb0557ad8e843d2172b43df747abb8b6
sytzez <sytzez@gmail.com>
2024-06-14 12:34:06 +0100

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,11 +11,24 @@ config.color_scheme = "Mathias"
 
 config.colors = {
 	background = "rgba(0 0 0 65%)",
+	tab_bar = {
+		background = "rgba(0 0 0 65%)",
+		inactive_tab = {
+			bg_color = "rgba(0 0 0 65%)",
+			fg_color = "#666666",
+		},
+		new_tab = {
+			bg_color = "rgba(0 0 0 65%)",
+			fg_color = "#666666",
+		},
+	},
 }
 
 config.font = wezterm.font("CaskaydiaCove NerdFont")
 config.font_size = 8
 config.freetype_load_target = "Normal"
+
+config.use_fancy_tab_bar = false
 
 -- and finally, return the configuration to wezterm
 return config
