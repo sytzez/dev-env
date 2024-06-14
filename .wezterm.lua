abed4e32f79b9cf01c6740272a72dec618d23e21
sytzez <sytzez@gmail.com>
2024-06-14 12:25:43 +0100

    Add .wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "Mathias"
+
+config.colors = {
+	background = "rgba(0 0 0 65%)",
+}
+
+config.font = wezterm.font("CaskaydiaCove NerdFont")
+config.font_size = 8
+config.freetype_load_target = "Normal"
+
+-- and finally, return the configuration to wezterm
+return config
