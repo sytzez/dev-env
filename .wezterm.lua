1acec4811b1c9d9e4dc1d993a78b2a0818b8f679
sytzez <sytzez@gmail.com>
2024-06-18 14:22:54 +0100

    Update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,6 +10,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "Mathias"
 
 config.colors = {
+	foreground = "white",
 	background = "rgba(0 0 0 65%)",
 	tab_bar = {
 		background = "rgba(0 0 0 65%)",
@@ -25,10 +26,14 @@ config.colors = {
 }
 
 config.font = wezterm.font("CaskaydiaCove NerdFont")
-config.font_size = 8
+config.font_size = 9
 config.freetype_load_target = "Normal"
 
 config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_decorations = "RESIZE"
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
 -- and finally, return the configuration to wezterm
 return config
