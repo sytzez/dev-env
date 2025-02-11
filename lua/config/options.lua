0c60cb58371eb4d7ca278ed77501ca9d8851489f
sytzez <sytzez@gmail.com>
2025-02-11 17:11:53 +0000

    disable anims and change default theme

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+
+-- Set to `false` to globally disable all snacks animations
+vim.g.snacks_animate = false
