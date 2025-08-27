fd1ee76a60d12d7c202a816883631f61a3e9f270
sytzez <sytzez@gmail.com>
2025-08-27 08:08:59 +0100

    colors

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,55 +1,63 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+config.color_scheme = "MaterialOcean"
 
--- For example, changing the color scheme:
-config.color_scheme = "Catppuccin Mocha"
+local bg = "#0f111A"
+local fg = "#8F93A2"
+local accent = "84ffff"
 
 config.colors = {
-    -- foreground = "white",
-    -- background = "rgba(0 0 0 50%)",
-    -- tab_bar = {
-    --      background = "rgba(0 0 0 65%)",
-    --      inactive_tab = {
-    --              bg_color = "rgba(0 0 0 65%)",
-    --              fg_color = "#666666",
-    --      },
-    --      new_tab = {
-    --              bg_color = "rgba(0 0 0 65%)",
-    --              fg_color = "#666666",
-    --      },
-    -- },
+	tab_bar = {
+		background = "none",
+		inactive_tab = {
+			bg_color = "none",
+			fg_color = fg,
+		},
+		active_tab = {
+			bg_color = bg,
+			fg_color = accent,
+		},
+		new_tab = {
+			bg_color = "none",
+			fg_color = fg,
+		},
+	},
 }
 
-local font = 3
+config.window_background_gradient = {
+	colors = { bg, "black" },
+	-- colors = { "#303030", "black" },
+	orientation = { Linear = { angle = -70.0 } },
+	noise = 64,
+}
+
+local font = 0
 if font == 1 then
-    -- Comfortable font
-    config.font = wezterm.font({
-        family = "0xProto",
-        harfbuzz_features = { "ss01" },
-    })
-    config.font_size = 11
+	-- Comfortable font
+	config.font = wezterm.font({
+		family = "0xProto",
+		harfbuzz_features = { "ss01" },
+	})
+	config.font_size = 11
 elseif font == 2 then
-    -- Most compact bitmap font, for looking at lots of code
-    config.font = wezterm.font_with_fallback({ "ProggySquareTTSZ", { family = "Symbols Nerd Font Mono", scale = 0.6 } })
-    config.font_size = 12
+	-- Most compact bitmap font, for looking at lots of code
+	config.font = wezterm.font_with_fallback({ "ProggySquareTTSZ", { family = "Symbols Nerd Font Mono", scale = 0.6 } })
+	config.font_size = 12
 elseif font == 3 then
-    -- Bit bigger bitmap font, more comfy
-    config.font = wezterm.font("Fixedsys Excelsior")
-    config.font_size = 12
+	-- Bit bigger bitmap font, more comfy
+	config.font = wezterm.font("Fixedsys Excelsior")
+	config.font_size = 12
 end
 
 config.freetype_load_target = "Normal"
 
 config.use_fancy_tab_bar = false
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
 -- config.tab_bar_at_bottom = true
 
-config.window_decorations = "RESIZE"
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
 -- and finally, return the configuration to wezterm
--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,23 +1,23 @@
 return {
-    {
-        "folke/tokyonight.nvim",
-        opts = { style = "night" },
-    },
-    {
-        "rebelot/kanagawa.nvim",
-        opts = {
-            colors = {
-                theme = {
-                    dragon = {
-                        ui = {
-                            bg_gutter = "none",
-                            bg = "none",
-                        },
-                    },
-                },
-            },
-        },
-    },
+    -- {
+    --     "folke/tokyonight.nvim",
+    --     opts = { style = "night" },
+    -- },
+    -- {
+    --     "rebelot/kanagawa.nvim",
+    --     opts = {
+    --         colors = {
+    --             theme = {
+    --                 dragon = {
+    --                     ui = {
+    --                         bg_gutter = "none",
+    --                         bg = "none",
+    --                     },
+    --                 },
+    --             },
+    --         },
+    --     },
+    -- },
     -- {
     --   "LazyVim/LazyVim",
     --   opts = {
@@ -29,4 +29,25 @@ return {
     --   main = "ibl",
     --   opts = {},
     -- },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "material-deep-ocean",
+        },
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        opts = {
+            theme = "material-stealth,",
+        },
+    },
+    {
+        "marko-cerovac/material.nvim",
+        priority = 1000,
+        opts = {
+            disable = {
+                background = true,
+            },
+        },
+    },
 }
